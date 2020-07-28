2aacf35bea8b54b67e05257265e18579e9982f46
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2020-07-28 18:31:01 +0200

    initial commit

---
+++ apps/nx-v10-scully-example-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-v10-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'nx-v10-scully-example-app';
+}
